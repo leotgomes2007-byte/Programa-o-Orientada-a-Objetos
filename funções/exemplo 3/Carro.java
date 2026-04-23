9a835477282157891473d2d74de83c586aa51661
Leonardo Gomes <leotgomes2007@gmail.com>
2026-04-23 13:27:18 -0300

    Create Carro.java

---
+++ funções/exemplo 3/Carro.java
@@ -0,0 +1,21 @@
+package projeto;
+
+public class Carro {
+	public String marca;
+	public String modelo;
+	
+	public void atribuirValores(String ma,String mo) {
+		marca = ma;
+		modelo = mo;
+	}
+	
+	public String retornaMarca() {
+		return marca;
+	}
+	
+	public String retornaModelo() {
+		return modelo;
+	}
+	
+
+}
