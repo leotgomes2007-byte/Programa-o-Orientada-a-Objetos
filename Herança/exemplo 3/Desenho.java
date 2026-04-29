65e6d0d6e3ff1c59c0d2baa250640f535347e6e5
Leonardo Gomes <leotgomes2007@gmail.com>
2026-04-29 08:35:47 -0300

    Add Desenho class with author name management

---
+++ Herança/exemplo 3/Desenho.java
@@ -0,0 +1,20 @@
+package project;
+
+public class Desenho {
+	protected String nomeAutor;
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public Desenho(String nomeAutor) {
+		super();
+		this.nomeAutor = nomeAutor;
+	}
+	
+	
+}
