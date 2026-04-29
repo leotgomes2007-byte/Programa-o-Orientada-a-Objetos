a440f195382dd218076a7af63c9e06e8cf7068eb
Leonardo Gomes <leotgomes2007@gmail.com>
2026-04-29 08:39:47 -0300

    Create computador.java

---
+++ lista4/exercicio 2/computador.java
@@ -0,0 +1,9 @@
+package lista4Exercicio2;
+
+public class computador {
+	public String marca = "DELL";
+	public String modelo = "Inspiron 15";
+	public String tipo = "notebook"; 
+	public String preco = "3100";
+
+}
