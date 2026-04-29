64ac261f8220b6f459fe29b436d731bcbdb4a029
Leonardo Gomes <leotgomes2007@gmail.com>
2026-04-29 08:37:29 -0300

    Add mainDesenho class to display Quadrado details

---
+++ Herança/exemplo 4/mainDesenho.java
@@ -0,0 +1,8 @@
+package project;
+
+public class mainDesenho {
+	public static void main(String[]args) {
+	Quadrado q = new Quadrado(100,150,"Ricardo","Quadrado do Ricardo");
+	q.exibeDados();
+	}
+}
