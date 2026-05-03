8a84da3f808c89841bb2070080075e7cfca010e4
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-02 21:31:15 -0300

    Create Animal.java

---
+++ lista8/exercicio 3/Animal.java
@@ -0,0 +1,14 @@
+package projeto;
+
+public class Animal {
+	protected String tipo;
+
+	public Animal(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void andar() {
+		System.out.println("Todos andam, mas o modo é variado");
+	}
+
+}
