44dde731f448a17565462467527c7b1936630270
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-02 21:28:32 -0300

    Add Lista8exercicio1.java for rectangle area calculation

---
+++ lista8/exercicio 1/Lista8exercicio1.java
@@ -0,0 +1,16 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class Lista8exercicio1 {
+
+	public static void main(String[] args) {
+
+		Retangulo r =new Retangulo("preto","sim",2,2);
+		
+		System.out.println("AREA: "+ r.calcularArea());
+		r.exibir();
+		
+	}
+
+}
