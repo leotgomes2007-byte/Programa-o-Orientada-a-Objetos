fd6fa5c42ca9fcdab27c068e7158171a3f7d4a56
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-02 21:27:58 -0300

    Add Figura class with color and fill properties

---
+++ lista8/exercicio 1/Figura.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class Figura {
+	protected String cor;
+	protected String preenchido;
+	
+	public Figura(String cor, String preenchido) {
+		this.cor = cor;
+		this.preenchido = preenchido;
+	}
+	
+	
+}
