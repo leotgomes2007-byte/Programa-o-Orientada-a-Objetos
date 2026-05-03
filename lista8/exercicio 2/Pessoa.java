6b151d759e1aebc4dda016f7eba3cbd74770723c
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-02 21:30:13 -0300

    Create Pessoa.java

---
+++ lista8/exercicio 2/Pessoa.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class Pessoa {
+	protected String nome;
+
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+
+	public void trabalhar() {
+		System.out.println("A pessoa está trabalhando");
+	}
+}
