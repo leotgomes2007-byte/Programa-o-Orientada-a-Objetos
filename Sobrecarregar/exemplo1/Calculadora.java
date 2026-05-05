834e273422455268cf5fcad6162dcd49306a7e63
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-05 10:32:43 -0300

    Create Calculadora.java

---
+++ Sobrecarregar/exemplo1/Calculadora.java
@@ -0,0 +1,24 @@
+package project;
+
+public class Calculadora {
+	
+	public int somar(int x,int y) {
+		return x+y;
+	}
+	
+	public int somar(int x,int y,int z) {
+		return x+y+z;
+	}
+	
+	public double somar(double x,double y) {
+		return x+y;
+	}
+	
+	public double multiplicar(double x,double y) {
+		return x+y;
+	}
+	
+	public int multiplicar(int x,int y) {
+		return x+y;
+	}
+}
