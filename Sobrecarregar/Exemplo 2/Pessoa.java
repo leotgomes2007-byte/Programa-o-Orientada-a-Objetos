a11b6f518864cd0057d0d7d7f15a2269a790a60a
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-05 10:23:02 -0300

    Create Pessoa.java

---
+++ Sobrecarregar/Exemplo 2/Pessoa.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Pessoa {
+
+	public void dizerOla() {
+		System.out.println("Olá!");
+	}
+	
+	public void dizerOla(String nome) {
+		System.out.println("Olá, "+nome);
+	}
+}
