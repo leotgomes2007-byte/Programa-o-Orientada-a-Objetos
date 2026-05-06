d21772e6e3def989adb3e0cca11eb452b1aeef98
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-06 08:12:56 -0300

    Add ContaBancaria class with calcularSaldo method

---
+++ Lista 9(Sobrescrita e Sobrecarregar)/exercicio 2/ContaBancaria.java
@@ -0,0 +1,9 @@
+package projeto;
+
+public class ContaBancaria {
+	
+	public void  calcularSaldo(double valor) {
+	double	saldo = 1000 + valor;
+	System.out.println("O seu saldo da ContaBancaria é "+saldo);
+	}
+}
