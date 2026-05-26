7ba7d2f543d96882aeae3d41c9a431ddc0ab8beb
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-26 12:37:03 -0300

    Create Funcionario.java

---
+++ Lista 10/Exercicio 6/Funcionario.java
@@ -0,0 +1,36 @@
+package project;
+
+public class Funcionario {
+	protected String nome;
+	protected double salario;
+	
+	
+	public Funcionario(String nome, double salario) {
+		super();
+		this.nome = nome;
+		this.salario = salario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public void aumentarSalario(double percentual) {
+		salario += salario*(percentual/100);
+		System.out.println("O salário do funcionario é:"+salario);
+	}
+	
+	@Override
+	public String toString() {
+		return "Funcionario [nome=" + nome + ", salario=" + salario + "]";
+	}
+	
+}
