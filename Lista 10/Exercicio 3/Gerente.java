b186478e66575250e118638424739ab470a1a63a
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-26 12:33:14 -0300

    Add Gerente class with salary calculation method

---
+++ Lista 10/Exercicio 3/Gerente.java
@@ -0,0 +1,9 @@
+package projeto;
+
+public class Gerente {
+	
+	public void calcularSalario(double salario ) {
+		double comissao = 500;
+		System.out.println("salario do Gerente(+ comissao): "+(salario+comissao)); 
+	}
+}
