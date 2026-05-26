a8a030efa2e5ea667582d325d7c8589a5cc4f1f1
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-26 12:38:39 -0300

    Create NotificacaoApp.java

---
+++ Lista 10/Exercicio 6/Exercicio 7/NotificacaoApp.java
@@ -0,0 +1,9 @@
+package project;
+
+public class NotificacaoApp {
+	
+	public void enviar(String mensagem,String destinatario) {
+		System.out.println("Notificação App");
+		System.out.println("A mensagem"+mensagem+"foi enviada para o destinatario"+destinatario);
+	}
+}
