de39ea1fa55a1dcfc74f45348b78ff87ad272654
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-26 12:48:19 -0300

    Create Livro.java

---
+++ Lista 10/Exercicio 12/Livro.java
@@ -0,0 +1,15 @@
+package project;
+
+public class Livro {
+	public String titulo;
+	public String autor;
+	public int ano;
+	
+	public Livro(String titulo,String autor,int ano) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ano = ano;
+		
+		
+	}
+}
