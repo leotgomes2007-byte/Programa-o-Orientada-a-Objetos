82f1c6adbaf0118031e9fe3666032735e2473641
Leonardo Gomes <leotgomes2007@gmail.com>
2026-05-26 12:38:26 -0300

    Add Notificacao class for sending notifications

---
+++ Lista 10/Exercicio 6/Exercicio 7/Notificacao.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Notificacao {
+
+	public void enviar(String mensagem,String destinatario) {
+		System.out.println("Notificação");
+		System.out.println("A mensagem"+mensagem+"foi enviada para o destinatario"+destinatario);
+	}
+}
