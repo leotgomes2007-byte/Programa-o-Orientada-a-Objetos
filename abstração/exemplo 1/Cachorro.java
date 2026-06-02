8feaaad5839f1968376a00f7fe243795d0a0e6c2
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-02 10:06:59 -0300

    Create Cachorro.java

---
+++ abstração/exemplo 1/Cachorro.java
@@ -0,0 +1,13 @@
+package poeject;
+
+public class Cachorro extends Animal{
+
+	@Override
+	public void emiteSom() {
+		System.out.println("au au au");
+	}
+
+	public void cuidarPatio() {
+		System.out.println("Cuidado!!! cão raivoso");
+	}
+}
