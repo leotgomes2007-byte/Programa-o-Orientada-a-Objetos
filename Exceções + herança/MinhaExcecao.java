e8750cd3f79cae7e877d1e2b584842ed16f82f93
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-09 10:01:10 -0300

    Create MinhaExcecao class extending Exception

---
+++ Exceções + herança/MinhaExcecao.java
@@ -0,0 +1,11 @@
+package project;
+
+public class MinhaExcecao extends Exception {    
+    public MinhaExcecao() {
+        super();
+    }
+    
+    public MinhaExcecao(String message) {
+        super(message);
+    }
+}
