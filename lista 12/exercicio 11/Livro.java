a0774ec216225ec04a1739decbca5964e65bb0b5
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-11 11:51:48 -0300

    Create Livro.java

---
+++ lista 12/exercicio 11/Livro.java
@@ -0,0 +1,23 @@
+class Livro implements Produto {
+    private String nome;
+    private float preco;
+    private String descricao;
+
+    public Livro(String nome, float preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
