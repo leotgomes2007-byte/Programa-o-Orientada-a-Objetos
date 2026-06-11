812d5942dea22dceea10e07ee02ba89b1b2602a1
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-11 12:20:31 -0300

    Add Exercicio6.java with Carro and Moto classes

---
+++ lista 12/Exercicio 6/Exercicio6.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Exercicio6 {
+
+	public static void main(String[] args) {
+		Carro c = new Carro("toyota","Supra mk4",2009);
+		c.Acelerar();
+		c.Frear();
+		
+		Moto m = new Moto("toyota","moto",1980);
+		m.Acelerar();
+		m.Frear();
+	}
+}
