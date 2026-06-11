81e663bea3c9a9fc38a15a884895cbea5833b0c0
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-11 12:16:36 -0300

    Create Exercicio4.java

---
+++ lista 12/exercicio 4/Exercicio4.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+    
+    	 Cachorro c  = new Cachorro("cusco",8);
+    	 c.EmitirSom();
+    	 
+    	 Gato g = new Gato("mingau",5);
+    	 g.EmitirSom();
+    }
+}
