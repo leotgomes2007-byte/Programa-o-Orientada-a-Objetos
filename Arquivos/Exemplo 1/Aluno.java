05df81aee3798948aeb8d5a556e8fe5bb0b647ce
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-16 08:47:34 -0300

    Create Aluno.java

---
+++ Arquivos/Exemplo 1/Aluno.java
@@ -0,0 +1,21 @@
+package pkg;
+
+import java.io.*;
+
+public class Aluno {
+	private String nome;
+	private int idade;
+	
+	public Aluno(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+}
