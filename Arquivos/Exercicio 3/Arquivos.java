3a9d95a017efc16064712840b4fd900955bd2b04
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-16 09:08:14 -0300

    Create Arquivos.java

---
+++ Arquivos/Exercicio 3/Arquivos.java
@@ -0,0 +1,72 @@
+package project;
+
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arquivos {
+    // Atributos baseados no Slide 14
+    private FileWriter arqw;
+    private BufferedWriter escritor;
+    private FileReader arqr;
+    private BufferedReader leitor;
+    private List<Aluno> listAlunos;
+    public String nomeArquivo;
+
+    // Construtor baseado no Slide 14
+    public Arquivos(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+        this.listAlunos = new ArrayList<>();
+    }
+
+    // Método para gravar dados baseado no Slide 13
+    public void gravaArquivo(Aluno a) {
+        try {
+            // Escrevendo os alunos no arquivo (o parâmetro 'true' ativa o modo append)
+            arqw = new FileWriter(nomeArquivo + ".txt", true);
+            escritor = new BufferedWriter(arqw);
+            
+            escritor.write(a.getNome() + "," + a.getIdade());
+            escritor.newLine();
+            
+            escritor.close();
+            arqw.close();
+            
+            System.out.println("Alunos salvos no arquivo alunos.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Método para ler dados baseado no Slide 15
+    public List<Aluno> leArquivo() {
+        System.out.println("Alunos lidos do arquivo:");
+        try {
+            arqr = new FileReader(nomeArquivo + ".txt");
+            leitor = new BufferedReader(arqr);
+            String linha;
+            
+            while ((linha = leitor.readLine()) != null) {
+                String[] campos = linha.split(",");
+                
+                String nome = campos[0];
+                int idade = Integer.parseInt(campos[1]);
+                
+                Aluno aluno = new Aluno(nome, idade);
+                listAlunos.add(aluno);
+            }
+            
+            leitor.close();
+            arqr.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return listAlunos;
+    }
+}
