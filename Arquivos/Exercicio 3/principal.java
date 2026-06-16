617c33287f586a3973bd651afcb6af7e5ca67764
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-16 09:10:13 -0300

    Update principal.java

--- Arquivos/Exercicio 3/principal.java
+++ Arquivos/Exercicio 3/principal.java
@@ -10,21 +10,16 @@ public class principal {
         Aluno aluno2 = new Aluno("Maria", 22);
         Aluno aluno3 = new Aluno("Alziras", 19);
         
-        // Criando a lista para receber os dados lidos (Slide 16)
         List<Aluno> lista = new ArrayList<>();
         
-        // Instanciando a classe Arquivo com o nome do arquivo "alunos" (Slide 16)
         Arquivos arquivo = new Arquivos("alunos");
         
-        // Gravando os três alunos no arquivo (Slide 16)
         arquivo.gravaArquivo(aluno1);
         arquivo.gravaArquivo(aluno2);
         arquivo.gravaArquivo(aluno3);
         
-        // Lendo os alunos do arquivo e armazenando o retorno na lista (Slide 16)
         lista = arquivo.leArquivo();
         
-        // Percorrendo a lista e exibindo os dados no console (Slide 16)
         for (Aluno a : lista) {
             System.out.println("Nome: " + a.getNome() + ", Idade: " + a.getIdade());
         }
