7879096d9df0e3374e359dc6d5efe33232134684
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-16 10:21:55 -0300

    Create Produto.java

---
+++ Serialização/exemplo 1/Produto.java
@@ -0,0 +1,38 @@
+package pkg;
+
+public class Produto implements Serializable {
+	private String codigo;
+	private String nome;
+	private double preco;
+	private transient String temporario;
+	
+	public Produto(String codigo, String nome, double preco, String temporario) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preco = preco;
+		this.temporario = temporario;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public String getTemporario() {
+		return temporario;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [codigo=" + codigo + ", nome=" + nome + ", preco=" + preco + "]";
+	}
+	
+}
