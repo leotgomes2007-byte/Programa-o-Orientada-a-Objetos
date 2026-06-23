0835b092f7d16cede5251261ebe8aee51cebb11d
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-23 09:16:15 -0300

    Create PrincipalServer.java

---
+++ Socket/exemplo 2/PrincipalServer.java
@@ -0,0 +1,31 @@
+public class PrincipalServer {
+ 
+	public static void main(String[] args) {
+		try {
+			ServerSocket servidor = new ServerSocket(1020);
+			System.out.println("Servidor criado com sucesso!");
+			
+			while(true) {
+				Socket cliente = servidor.accept();
+				ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
+				ObjectInputStream in = new ObjectInputStream(cliente.getInputStream());
+				
+				Numero n; //declarando, pois os valores de dentro da classe vem do cliente
+				Resposta res;
+				
+				n = (Numero) in.readObject(); //leio o objeto do cliente, fazendo um cast
+				
+				System.out.println("Li do cliente: "+n.getX()+" e "+n.getY());
+				
+				float resultado = n.getX() + n.getY();
+				res = new Resposta(resultado);
+				out.writeObject(res);
+				cliente.close();
+				
+			}
+		}catch(Exception e) {
+			System.out.println("Erro na criação do servidor: "+e.getMessage());
+		}
+	}
+}
+ 
