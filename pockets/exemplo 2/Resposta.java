1ea5d946c8f477bde9a0ed8ef2e3c76c82e40cc5
Leonardo Gomes <leotgomes2007@gmail.com>
2026-06-23 09:00:22 -0300

    Create Resposta.java

---
+++ pockets/exemplo 2/Resposta.java
@@ -0,0 +1,17 @@
+public class Resposta {
+	private float resultado;
+ 
+	public Resposta(float resultado) {
+		super();
+		this.resultado = resultado;
+	}
+ 
+	public float getResultado() {
+		return resultado;
+	}
+ 
+	public void setResultado(float resultado) {
+		this.resultado = resultado;
+	}
+	
+}
